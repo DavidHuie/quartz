278dde97cd269737c58932f5fb632a38cb15dab3
David Huie <david@nationbuilder.com>
2014-06-17 11:45:33 -0700

    Cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,29 +1,9 @@
 require 'simplecov'
 
-module SimpleCov::Configuration
-  def clean_filters
-    @filters = []
-  end
-end
+ENV['COVERAGE'] && SimpleCov.start
 
-SimpleCov.configure do
-  clean_filters
-  load_adapter 'test_frameworks'
-end
-
-ENV["COVERAGE"] && SimpleCov.start do
-  add_filter "/.rvm/"
-end
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'rspec'
 require 'quartz'
-
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-
-RSpec.configure do |config|
-
-end
