77cb49f7a924d3abb14b93e3224dbfb884d6232d
David Huie <david@nationbuilder.com>
2014-06-19 00:33:02 -0700

    Go check

--- lib/quartz.rb
+++ lib/quartz.rb
@@ -1,10 +1,15 @@
 require 'json'
 require 'socket'
 
-module Quartz
-
-end
+module Quartz; end
 
 require 'quartz/go_process'
 require 'quartz/go_struct'
 require 'quartz/client'
+
+# Check if go is installed
+go_exists = ENV['PATH'].split(File::PATH_SEPARATOR).any? do |directory|
+  File.exist?(File.join(directory, 'go'))
+end
+
+raise 'Go not installed' unless go_exists
