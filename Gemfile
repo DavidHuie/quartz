ed242559c66501444dbe1da5e0ec317346bef2fa
David Huie <dahuie@gmail.com>
2015-01-27 17:40:25 -0800

    Profiling helper

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'http://rubygems.org'
 
 group :development do
-  gem 'rspec', '~> 3.1.0'
   gem 'jeweler', '~> 2.0.1'
+  gem 'rspec', '~> 3.1.0'
+  gem 'ruby-prof', '~> 0.15.3'
   gem 'simplecov', '~> 0.9.1'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+require 'ruby-prof'
 require 'simplecov'
 
 ENV['COVERAGE'] && SimpleCov.start
@@ -7,3 +8,17 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'rspec'
 require 'quartz'
+
+def profile(wall_time = false, &block)
+  if wall_time
+    RubyProf.measure_mode = RubyProf::WALL_TIME
+  end
+  RubyProf.start
+  call_result = block.call
+  result = RubyProf.stop
+  File.open('profile', 'w') do |f|
+    printer = RubyProf::FlatPrinter.new(result)
+    printer.print(f)
+  end
+  return call_result
+end
