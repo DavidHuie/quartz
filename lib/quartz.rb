85bbdb5a7f2bab808eeab5faa5830c99d5d28ed8
David Huie <dahuie@gmail.com>
2015-03-01 14:21:43 -0800

    Don't validate GOPATH (fixes #25)

--- lib/quartz.rb
+++ lib/quartz.rb
@@ -2,14 +2,10 @@ require 'json'
 require 'securerandom'
 require 'socket'
 
-module Quartz
-end
+module Quartz; end
 
 require 'quartz/exceptions'
 require 'quartz/go_process'
 require 'quartz/go_struct'
 require 'quartz/client'
 require 'quartz/validations'
-
-Quartz::Validations.check_for_go
-Quartz::Validations.check_go_quartz_version
--- lib/quartz/validations.rb
+++ lib/quartz/validations.rb
@@ -1,36 +1,11 @@
 module Quartz::Validations
 
-  GO_FILE_LOCATION = 'src/github.com/DavidHuie/quartz/go/quartz/quartz.go'
-
   def self.check_for_go
     go_exists = ENV['PATH'].split(File::PATH_SEPARATOR).any? do |directory|
       File.exist?(File.join(directory, 'go'))
     end
 
     raise Quartz::ConfigError, 'Go not installed.' unless go_exists
-  end
-
-  def self.check_go_quartz_version
-    current_quartz = File.read(File.join(File.dirname(__FILE__), '../../go/quartz/quartz.go'))
-
-    installed_quartz_dir = ENV['GOPATH'].split(File::PATH_SEPARATOR).select do |directory|
-      File.exist?(File.join(directory, Quartz::Validations::GO_FILE_LOCATION))
-    end[0]
-
-    unless installed_quartz_dir
-      raise Quartz::ConfigError, "GOPATH not configured."
-    end
-
-    installed_quartz = File.read(File.join(installed_quartz_dir,
-                                          Quartz::Validations::GO_FILE_LOCATION))
-
-    if current_quartz != installed_quartz
-      STDERR.write <<-EOS
-Warning: the version of Quartz in $GOPATH does not match
-the version packaged with the gem. Please update the Go
-Quartz package.
-EOS
-    end
   end
 
 end
